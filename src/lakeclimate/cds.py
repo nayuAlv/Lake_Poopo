e317c05cc1103218dce3e1c402590b3f1eaeaebb
nayuAlv <alvarez.nayra@mailbox.org>
2026-06-24 17:49:16 +0200

    cds notebook update

--- src/lakeclimate/cds.py
+++ src/lakeclimate/cds.py
@@ -14,14 +14,13 @@ def area_weighted_mean(ds: xr.Dataset, var: str) -> xr.DataArray:
     weighted = ds[var].weighted(weights)
     return weighted.mean(dim=("latitude", "longitude"))
 
-def era5_monthly_to_mm(ds, var, unit_convertion):
+def era5_monthly_accum(ds, var, unit_convertion):
     """
     ERA5 monthly-means: values are m/day (daily mean for the month).
     To get monthly accumulation in mm:
       value [m/day] * days_in_month * 1000 [mm/m] = mm/month
     """
     weights = np.cos(np.deg2rad(ds["latitude"]))
-    #weights = weights / weights.sum()
     weighted_mean = (ds[var].weighted(weights).mean(dim=("latitude", "longitude")))
     df = weighted_mean.to_dataframe().reset_index()
     days = pd.to_datetime(df["valid_time"]).dt.days_in_month
